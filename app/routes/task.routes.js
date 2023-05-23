8d751b27b7b6ba2d7bc444e5c7ad683b3b229efe
Sheruni Fernando <sheruni.fernando@velaris.io>
2023-05-23 13:51:25 +0530

    add express router

--- app/routes/task.routes.js
+++ app/routes/task.routes.js
@@ -1,10 +1,16 @@
+const express = require('express');
+const router = express.Router();
 const tasks = require('../controllers/task.controller.js');
 const { validateTask, validateUpdateTask } = require('../validators/taskValidator.js');
 
-module.exports = function(app) {
-  app.post('/tasks', validateTask, tasks.create);
-  app.get('/tasks', tasks.findAll);
-  app.get('/tasks/:taskId', tasks.findOne);
-  app.put('/tasks/:taskId', validateUpdateTask, tasks.update);
-  app.delete('/tasks/:taskId', tasks.delete);
-};
+router.post('/', validateTask, tasks.create);
+router.get('/', tasks.findAll);
+router.get('/:taskId', tasks.findOne);
+router.put('/:taskId', validateUpdateTask, tasks.update);
+router.delete('/:taskId', tasks.delete);
+
+module.exports = router;
+
+
+
+
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 
 const app = express();
-const taskRoute = require('./app/routes/task.routes.js');
+const taskRouter = require('./app/routes/task.routes.js');
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-taskRoute(app);
+// Using the router as a middleware
+app.use('/tasks', taskRouter);
 
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
